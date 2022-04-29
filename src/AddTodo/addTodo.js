3e8a4907bed883960223e471b27ecc59c0bffa18
eder.lopez <eder.lopez@designory.com>
2022-04-29 13:12:02 -0500

    Initial

---
+++ src/AddTodo/addTodo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class AddTodo extends React.Component {
+    constructor () {
+      super();
+      this.state = {
+        todo: ''
+      }
+    }
+    
+    render() {
+      return(
+        <div className='addTodoContainer'>
+          <form onSubmit={(e) => this.submitTodo(e)}>
+            <input id='addTodoInput' type='text' onChange={(e) => this.updateInput(e)}></input>
+            <button type='submit'>Add Todo</button>
+          </form>
+        </div>
+      )
+    }
+
+    updateInput = (e) => {
+      this.setState({ todo: e.target.value });
+    }
+
+    submitTodo = (e) => {
+        e.preventDefault();
+        this.props.addTodoFn(this.state.todo);
+        document.getElementById('addTodoInput').value = '';
+      }
+  }
+  
+export default AddTodo;
